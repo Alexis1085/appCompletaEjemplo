c497c665305b4646e1511a3b716d98bad9f2e99c
Alexis <aleiacusso@hotmail.com>
2022-12-09 02:37:58 -0300

    Configuración inicial de la App Completa de Ejemplo terminada

---
+++ appcompleta.sql
@@ -0,0 +1,5 @@
+
+-- Base de Datos para la App Completa de Ejemplo:
+create database empresautn;
+
+use empresautn;
---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+
+const mysql = require('mysql2');
+const hbs = require('hbs'); //* Motor de Plantilla
+const path = require('path'); //* Enrutador para encontrar archivos
+const nodemailer = require('nodemailer'); //* Para enviar mails
+require('dotenv').config(); //? Variables de Entorno. La librería dotenv no es necesaria traerla a una variable, sólo aplicamos la configuración --> "Loads .env file contents into process.env"
+
+const port = process.env.SERVER_PORT || 9000;
+console.log(`Puerto del servidor: ${port}`);
+
+//* Middlewares:
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//? Configuramos el Frontend: Motor de Plantillas de HBS
+app.set('view engine', 'hbs');
+// Configuramos la ubicación de las plantillas
+app.set('views', path.join(__dirname, 'views'));
+// Configuramos los parciales de los motores de plantillas
+hbs.registerPartials(path.join(__dirname, 'views/partials'))
+
+//* Conexión a la Base de Datos
+const conexion = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+conexion.connect(err => {
+    if (err) throw err;
+    console.log(`Conectado a la Base de Datos: ${process.env.DB_NAME}`);
+});
+
+
+//* Rutas de la aplicación
+app.get('/',(req,res) => {
+    res.send('Bienvenido a la App Completa');
+});
+
+
+
+//* Servidor a la escucha de las peticiones
+app.listen(port, () => {
+    console.log(`Servidor conectado al puerto ${port}`);
+});
